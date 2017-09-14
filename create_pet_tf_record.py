eb153c668c25b0cdb0ec57428c0d9e34e2ee4e5b
MarilynWaldman <mdwaldman22@hotmail.com>
2017-09-14 14:39:57 -0600

    added trainval

--- create_pet_tf_record.py
+++ create_pet_tf_record.py
@@ -64,7 +64,7 @@ def get_class_name_from_filename(file_name):
 def dict_to_tf_example(data,
                        label_map_dict,
                        image_subdirectory,
-                       ignore_difficult_instances=False):
+                       ignore_difficult_instances=True):
   """Convert XML derived dict to tf.Example proto.
 
   Notice that this function normalizes the bounding box coordinates provided
